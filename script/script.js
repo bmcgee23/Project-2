3bfe005be6330d28e970c811df9ec1c814fff498
Darthseid <jared.mcvey@yahoo.com>
2018-11-11 03:02:09 -0600

    Added event listeners for future use

--- script/script.js
+++ script/script.js
@@ -1,2 +1,12 @@
 var submission = document.getElementById('submit');
 
+function submitBtntester() 
+{
+ 
+  var firstname = document.getElementById('fname');
+  var lastname = document.getElementById('lname');
+  var email = document.getElementById('email');
+  var phone = document.getElementById('number');
+  var dob = document.getElementById('bday');
+  
+};
